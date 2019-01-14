8c7f070bcf57810b37a2998cf4933936db1c84fa
Armen G <gharibi.armen@yahoo.com>
2019-01-13 19:30:26 -0800

    Add Spring Security Authentication + Auth0 third party authentication source

--- bike-ui/src/app/app.module.ts
+++ bike-ui/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BikeService } from './services/bike.service';
-import { AdminComponent } from './components/admin/admin.component';
-import { HomeComponent } from './components/home/home.component';
-import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BikeService} from './services/bike.service';
+import {AdminComponent} from './components/admin/admin.component';
+import {HomeComponent} from './components/home/home.component';
+import {ViewRegistrationComponent} from './components/view-registration/view-registration.component';
+import {CallbackComponent} from './components/callback/callback.component';
+import {AuthService} from './services/auth.service';
+import {AuthGuard} from './services/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     HomeComponent,
-    ViewRegistrationComponent
+    ViewRegistrationComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +27,8 @@ import { ViewRegistrationComponent } from './components/view-registration/view-r
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [BikeService],
+  providers: [BikeService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- bike-ui/src/app/services/bike.service.ts
+++ bike-ui/src/app/services/bike.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
 @Injectable({
@@ -16,11 +15,17 @@ export class BikeService {
   }
 
   getBikes() {
-    return this.http.get('/server/api/v1/bikes');
+    let token = localStorage.getItem('access_token');
+    return this.http.get('/server/api/v1/bikes',
+      {headers: new HttpHeaders().set('Authorization', 'Bearer' + token)}
+    );
   }
 
   getBike(id: number) {
-    return this.http.get('/server/api/v1/bikes/' + id);
+    let token = localStorage.getItem('access_token');
+    return this.http.get('/server/api/v1/bikes/' + id,
+      {headers: new HttpHeaders().set('Authorization', 'Bearer' + token)}
+    );
   }
 
   createBikeRegistration(bike) {
